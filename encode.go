97ed6685a2e6730fad316dbf6e8353ec87886f97
Callum Jones <cj@icj.me>
2022-10-11 14:46:42 +0100

    encode: multiselect encoding now checks if options are selected based on the current (#21)
    multiselect value (fixes #14)

--- encode.go
+++ encode.go
@@ -601,7 +601,24 @@ func BuildSelectField(s Select, key string, field StructField) *html.Node {
 		checked := false
 
 		if opt.Checked == nil {
-			checked = toString(opt.Value) == toString(s)
+			v := reflect.ValueOf(s)
+			optValue := toString(opt.Value)
+
+			switch v.Kind() {
+			case reflect.Slice, reflect.Array:
+				for i := 0; i < v.Len(); i++ {
+					val := v.Index(i)
+
+					if val.CanInterface() {
+						if toString(val.Interface()) == optValue {
+							checked = true
+							break
+						}
+					}
+				}
+			default:
+				checked = toString(s) == optValue
+			}
 		} else {
 			checked = bool(*opt.Checked)
 		}
